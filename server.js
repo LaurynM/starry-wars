b5668f863d997bc6c626ca59dfb2721717408a50
Lauryn McFarland <lauryn@Lauryns-MacBook-Pro.local>
2018-01-29 21:00:56 -0600

    changed port var again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 // =============================================================
 var app = express();
 // var PORT = 3000;
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -92,6 +92,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(port, function() {
+app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
